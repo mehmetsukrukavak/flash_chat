928059dae5675ddb14310bd96a632f3c69c2a4ba
Mehmet Şükrü Kavak <mehmetsukru.kavak07@gmail.com>
2022-11-02 23:04:15 +0300

    added IQKeyboard

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -96,9 +96,11 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let message = messages[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
         
-        cell.label.text = messages[indexPath.row].body
+        cell.label.text = message.body
         
         return cell
     }
